cc670080823bf2fa364b47a587af8536c5d851d0
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-08-01 05:51:31 -0300

    Criado função com parametros e argumentos e tambem estrutura de decisão if e else

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Ola Mundo!")
+let saldoRanked = qtdTaxaVitoriaEDerrota(40, 9)
+
+
+
+if(saldoRanked <= 10){
+  console.log("Parabens você seu rank é Ferro!")
+} else if(saldoRanked >= 11 || saldoRanked <= 20){
+  console.log("Parabens você agora é bronze!")
+} else if(saldoRanked >= 21 || saldoRanked <= 50){
+  console.log("Parabens você agora é Prata!")
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function qtdTaxaVitoriaEDerrota(vitoria, derrota){
+  let subPartidas = vitoria - derrota
+  return subPartidas
+}
