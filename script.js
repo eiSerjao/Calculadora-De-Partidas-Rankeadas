d204ebbbae2ca341849eea27d3c6d2dd2959693e
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-08-01 06:15:12 -0300

    Estrutura de decisão feita

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let saldoRanked = qtdTaxaVitoriaEDerrota(40, 9)
+let saldoRanked = qtdTaxaVitoriaEDerrota(200, 99)
 
 
 
 if(saldoRanked <= 10){
   console.log("Parabens você seu rank é Ferro!")
-} else if(saldoRanked >= 11 || saldoRanked <= 20){
+} else if(saldoRanked >= 11 && saldoRanked <= 20){
   console.log("Parabens você agora é bronze!")
-} else if(saldoRanked >= 21 || saldoRanked <= 50){
+} else if(saldoRanked >= 21 && saldoRanked <= 50){
   console.log("Parabens você agora é Prata!")
-} 
+} else if(saldoRanked >= 51 && saldoRanked <= 80){
+  console.log("Parabens você agora é Ouro!")
+} else if(saldoRanked >= 81 && saldoRanked <= 100){
+  console.log("Parabens você agora é Lendário!")
+} else if(saldoRanked >= 101){
+  console.log("Parabens você agora é Imortal!")
+}
 
 
 
