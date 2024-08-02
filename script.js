00a18834876f7f40b1b9d02eb71c155c1e8143ca
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-08-01 21:38:14 -0300

    Algoritmo Pronto!

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-// let winLose = [
-//   [20,11]
-//   [20,1]
-//   [50,1]
-//   [80,1]
-//   [100,1]
-//   [200,1]
-// ]
-for(){
-  let saldoRanked = qtdTaxaVitoriaEDerrota(50,1)
+let winLose = [
+  [9,20],
+  [19,35],
+  [49,78],
+  [79,60],
+  [99,4],
+  [199,80]
+]
 
+for(let contador = 0; contador < winLose.length; contador++){
+  
+  let win = winLose[contador][0]
+  
+  let lose = winLose[contador][1]
+  
+  let saldoRanked = qtdTaxaVitoriaEDerrota(win , lose)
 
+  let rank
+  
+    if(win < 10){
+      rank = ("Ferro")
 
-if(saldoRanked <= 10){
-  console.log("Parabens você seu rank é Ferro!")
+    } else if(win >= 11 && win <= 20){
+      rank = ("Bronze")
 
-} else if(saldoRanked >= 11 && saldoRanked <= 20){
-  console.log("Parabens você agora é bronze!")
+    } else if(win >= 21 && win <= 50){
+      rank = ("Prata")
 
-} else if(saldoRanked >= 21 && saldoRanked <= 50){
-  console.log("Parabens você agora é Prata!")
+    } else if(win >= 51 && win <= 80){
+      rank = ("Ouro")
 
-} else if(saldoRanked >= 51 && saldoRanked <= 80){
-  console.log("Parabens você agora é Ouro!")
+    } else if(win >= 81 && win <= 100){
+      rank = ("Lendario")
 
-} else if(saldoRanked >= 81 && saldoRanked <= 100){
-  console.log("Parabens você agora é Lendário!")
+    } else if(win >= 101){
+      rank = ("Imortal")
+    }
 
-} else if(saldoRanked >= 101){
-  console.log("Parabens você agora é Imortal!")
-}
-}
+    console.log(`O Heroi está no com saldo de ${saldoRanked} e por isso seu rank é ${rank}` )
+    
+    
+  }
 
-
-function qtdTaxaVitoriaEDerrota(vitoria, derrota){
-  let subPartidas = vitoria - derrota
-  return subPartidas
-}
+  
+  function qtdTaxaVitoriaEDerrota(win, lose){
+    let subPartidas = win - lose
+    return subPartidas
+  }
+  
